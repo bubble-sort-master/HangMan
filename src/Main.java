3c565be2c5770b06ac2ecdee930284e532a3077d
Gleb <glebelopov22@gmail.com>
2025-09-26 00:48:34 +0300

    1)corrected naming 2)corrected exception handling 3)changed some code logic

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,45 @@ import java.util.*;
 
 public class Main {
 
-    private static int WORD_MAX_LENGTH = 7;
+    private static final int WORD_LENGTH = 7;
 
-    private static String GAME_STATE_PLAYER_LOST = "К сожалению, вы проиграли";
-    private static String GAME_STATE_PLAYER_WON = "Вы отгадали  слово!";
+    private static final String GAME_STATE_PLAYER_LOST = "К сожалению, вы проиграли";
+    private static final String GAME_STATE_PLAYER_WON = "Вы отгадали  слово!";
 
-    private static String WRONG_GUESS = "Буквы в слове нет";
-    private static String CORRECT_GUESS = "Буква есть в слове";
-    private static int MAX_MISTAKES = 7;
+    private static final String WRONG_GUESS = "Буквы в слове нет";
+    private static final String CORRECT_GUESS = "Буква есть в слове";
+    private static final int MAX_MISTAKES = 7;
 
-    private static Scanner scanner = new Scanner(System.in);
-    private static Random random = new Random();
+    private static final String START = "1";
+    private static final String QUIT = "2";
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
 
     public static void main(String[] args) {
         System.out.println();
         System.out.println("\t\tДобро пожаловать в игру виселица!\n");
         System.out.println("Все слова загадываются на русском языке, длина каждого слова - 7; Регистр ввода не важен\n" +
                 "седьмая ошибка будет последней :)");
-        ArrayList<String> dictionary = initialiseDictionary();
 
         do {
             System.out.println();
-            System.out.println("Чтобы начать новую игру -- введите 1");
-            System.out.println("Чтобы покинуть игру  -- введите 2");
+            System.out.println("Чтобы начать новую игру -- введите " + START);
+            System.out.println("Чтобы покинуть игру  -- введите "+  QUIT);
             String userChoice = scanner.nextLine();
             System.out.println();
 
             switch (userChoice) {
-                case "1":
-                    System.out.println("Было загадано новое слово");
-                    runNewGameLoop(dictionary);
+                case START:
+                    try {
+                        ArrayList<String> dictionary = createDictionary();
+                        System.out.println("Было загадано новое слово");
+                        start(dictionary);
+                    }catch (IOException e){
+                        return;
+                    }
                     break;
-                case "2":
+                case QUIT:
                     System.out.println("До новых встреч!");
                     return;
                 default:
@@ -46,7 +53,7 @@ public class Main {
         }while (true);
     }
 
-    public static void runNewGameLoop(ArrayList<String> dictionary){
+    public static void start(ArrayList<String> dictionary){
 
         String wordToGuess = getRandomWord(dictionary).toUpperCase();
 
@@ -54,50 +61,51 @@ public class Main {
         ArrayList<String> wrongGuesses = new ArrayList<>();
         int mistakeCount = 0;
 
-        HangManPicture.displayHangMan(mistakeCount);
+        HangmanPicture.printHangman(mistakeCount);
 
-        do{
+        while (true){
             String userGuessStatus = processUserGuess(wordToGuess, correctGuesses, wrongGuesses);
-                if (Objects.equals(userGuessStatus, WRONG_GUESS))
-                    mistakeCount++;
+            if (Objects.equals(userGuessStatus, WRONG_GUESS))
+                mistakeCount++;
 
-             HangManPicture.displayHangMan(mistakeCount);
-             printWordProgress(wordToGuess, correctGuesses,wrongGuesses,mistakeCount);
+            HangmanPicture.printHangman(mistakeCount);
 
-             if(mistakeCount == MAX_MISTAKES){
-                 System.out.println(GAME_STATE_PLAYER_LOST);
-                 System.out.println("Загаданное слово: "+ wordToGuess);
-                 return;
-             }
-             else if(isWordSolved(wordToGuess, correctGuesses)){
-                 System.out.println(GAME_STATE_PLAYER_WON);
-                 int a = 1213;
-                 return;
-             }
+            printWordState(wordToGuess,correctGuesses);
+            printTotalMistakes(wrongGuesses,mistakeCount);
 
-        }while (true);
+            if(mistakeCount == MAX_MISTAKES){
+                System.out.println(GAME_STATE_PLAYER_LOST);
+                System.out.println("Загаданное слово: "+ wordToGuess);
+                return;
+            }
+            if(isWordSolved(wordToGuess, correctGuesses)){
+                System.out.println(GAME_STATE_PLAYER_WON);
+                return;
+            }
+
+        }
 
     }
 
-    private static void printWordProgress(String wordToGuess, ArrayList<String> correctGuesses, ArrayList<String> wrongGuesses, int mistakeCount) {
-        System.out.println("Текущее состояние слова:");
+    public static ArrayList<String> createDictionary() throws IOException {
+        ArrayList<String> dictionary = new ArrayList<>();
+        File file = new File("dictionary.txt"); // Создаем объект File
 
-         for(int i=0;i<WORD_MAX_LENGTH;i++){
-             if(correctGuesses.contains(Character.toString(wordToGuess.charAt(i)))){
-                 System.out.print(wordToGuess.charAt(i));
-             }
-             else
-                 System.out.print("_");
-         }
-        System.out.println();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                dictionary.add(line);
+            }
+        } catch (IOException e) {
+            // Выводим абсолютный путь файла
+            System.out.println("Не удалось открыть файл по пути: " + file.getAbsolutePath());
+            throw new IOException();
+        }
+        return dictionary;
+    }
 
-         if(mistakeCount>0){
-             System.out.println("Колличество ошибок: " + mistakeCount);
-             System.out.print("Ошибки: ");
-             for(String s: wrongGuesses)
-                 System.out.print(s + " ");
-             System.out.println("\n");
-         }
+    public static String getRandomWord(ArrayList<String> dictionary){
+        return dictionary.get(random.nextInt(dictionary.size()));
     }
 
     public static String processUserGuess(String wordToGuess, ArrayList<String> correctGuesses, ArrayList<String> wrongGuesses){
@@ -116,7 +124,7 @@ public class Main {
                 continue;
             }
 
-            for (int i = 0; i < WORD_MAX_LENGTH; i++) {
+            for (int i = 0; i < WORD_LENGTH; i++) {
                 if (guessedLetter.charAt(0) == wordToGuess.charAt(i)) {
                     System.out.println("Есть такая буква!");
                     correctGuesses.add(guessedLetter);
@@ -126,28 +134,9 @@ public class Main {
             }
             System.out.println("Такой буквы в слове нет!");
             wrongGuesses.add(guessedLetter);
-             return WRONG_GUESS;
+            return WRONG_GUESS;
         }
 
-    }
-
-    public static ArrayList<String> initialiseDictionary(){
-        ArrayList<String> dictionary = new ArrayList<>();
-
-        try(BufferedReader reader = new BufferedReader(new FileReader("dictionary.txt"))){
-            String line;
-            while ((line = reader.readLine())!=null){
-                dictionary.add(line);
-            }
-        }catch (IOException e){
-            System.out.println("Проблема при инитиализации словаря:");
-            e.printStackTrace();
-        }
-        return dictionary;
-    }
-
-    public static String getRandomWord(ArrayList<String> dictionary){
-         return dictionary.get(random.nextInt(dictionary.size()));
     }
 
     public static boolean isRussianLetter(String letter){
@@ -160,10 +149,33 @@ public class Main {
 
     public static boolean isWordSolved(String guessedWord, ArrayList<String> correctGuesses){
         char[] word = guessedWord.toCharArray();
-        for(char l: word){
-            if(!correctGuesses.contains(Character.toString(l)))
+        for(char c: word){
+            if(!correctGuesses.contains(Character.toString(c)))
                 return false;
         }
         return true;
+    }
+
+    public static void printWordState(String wordToGuess, ArrayList<String> correctGuesses){
+        System.out.println("Текущее состояние слова:");
+
+        for(int i = 0; i< WORD_LENGTH; i++){
+            if(correctGuesses.contains(Character.toString(wordToGuess.charAt(i)))){
+                System.out.print(wordToGuess.charAt(i));
+            }
+            else
+                System.out.print("_");
+        }
+        System.out.println();
+    }
+
+    public static void printTotalMistakes(ArrayList<String> wrongGuesses, int mistakeCount){
+        if(mistakeCount>0){
+            System.out.println("Колличество ошибок: " + mistakeCount);
+            System.out.print("Ошибки: ");
+            for(String s: wrongGuesses)
+                System.out.print(s + " ");
+            System.out.println("\n");
+        }
     }
 }
